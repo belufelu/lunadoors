789272f3842bd54f1bfe853f47703df9e8098a81
belufelu <belufelu1@gmail.com>
2025-11-29 15:32:56 +0300

    Create lunaloader.lua

---
+++ lunaloader.lua
@@ -0,0 +1,10 @@
+if game.PlaceId == 6839171747 then
+    local floor = game:GetService("ReplicatedStorage").GameData.Floor.Value
+
+    if floor == "Hotel" then
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/belufelu/lunadoors/refs/heads/main/luna.lua'))()
+
+    elseif floor == "Rooms" then
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/belufelu/lunadoors/refs/heads/main/luna.lua'))()
+    end
+end
